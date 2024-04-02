2292a2baf1b652ce9ecf97c4f974a5c9835f5788
Hugo Swerdtfegers <130606953+HugoSwerdtfegers@users.noreply.github.com>
2024-04-02 13:45:56 +0200

    Update run_all_DOC.sh

--- run_all_DOC.sh
+++ run_all_DOC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
-for scenario in reference optimistic conservative; do
+for scenario in reference optimistic conservative variable_DOC; do
     echo "Simulation launch for $scenario scenario"    
     python3 main_DOC.py --scenario $scenario
     echo ""
